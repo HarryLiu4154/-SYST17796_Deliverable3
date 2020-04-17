a18215b4b4d14f5eea2423c81645f88c4daf68fd
HarryLiu4154 <harryliu4154@hotmail.com>
2020-04-17 02:53:41 -0400

    Added Ranks and Suits

---
+++ src/com/sheridan/War/Ranks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sheridan.War;
+
+/**
+ *
+ * @author Harry
+ */
+public enum Ranks {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
