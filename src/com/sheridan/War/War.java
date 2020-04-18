b58470096107ea47ceed5decc53fc415d34935db
aksal <aksal@LAPTOP-K19K3R1R>
2020-04-18 16:58:44 -0400

    war game

---
+++ src/com/sheridan/War/War.java
@@ -0,0 +1,42 @@
+/*
+        Name:  [your name here]
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+
+package com.sheridan.War;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aksal
+ */
+public class War {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        String player1;
+        String player2;
+        String option;
+        String roundwinner;
+        String gamewinner;
+        int temp;
+        int play =1;
+        boolean winner;
+        
+        while(play !=0){
+            winner= false;
+            Deck deck = new Deck();
+            deck.shuffle(play);
+        }
+    }
+
+}
